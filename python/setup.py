7ef2900c43e2b2b2f2282d6750d472684b59e715
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-10-22 10:16:57 -0300

    Actualizo typestubs

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-   version="1.0.3",
+   version="1.0.5",
    name="fundar",
    author="Fundar",
    description="Private Python library.",
    packages=find_packages(),
+   package_data={'fundar': ['py.typed', '*.pyi', '**/*.pyi']},
    include_package_data=True,
    python_requires='>=3.10',
    setup_requires=['setuptools-git-versioning'],
